92e89b01add3ece86e021ce18cc8b85d27fa42c2
Aniket Bamotra <aniketbamotra@gmail.com>
2022-03-28 16:03:19 +0530

    added side menu

---
+++ index.js
@@ -0,0 +1,5 @@
+let bio = document.getElementById("im");
+let proj = document.getElementById("work-sec-title");
+let contact = document.getElementById("contact-title");
+var rect = contact.getBoundingClientRect();
+console.log(rect.top);
